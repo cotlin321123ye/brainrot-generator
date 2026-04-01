b90f49533c6e4e54677deaf8d4cbb2934c3d0d3f
cotlin321123ye <qqqrrrr222@gmail.com>
2026-04-02 01:08:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,5 @@
-// ВСЕ БРЕЙНРОТЫ
-const allBrainrots = [
-    "Meowl", "Strawberry Elephant", "Headless Horseman", "Skibidi Toilet",
-    "Griffin", "Hydra Dragon Cannelloni", "Dragon Gingerini", "Dragon Cannelloni",
-    "Love Love Bear", "La Supreme Combinasion", "Celestial Pegasus", "Cerberus",
-    "Popcuru and Fizzuru", "Rosey and Teddy", "Capitano Moby", "Cooki and Milki",
-    "Burguro And Fryuro", "Ketupat Bros", "Reinito Sleighito", "Fortunu and Cashuru",
-    "Los Amigos", "La Secret Combinasion", "Los Sekolahs", "Signore Carapace",
-    "La Casa Boo", "Fragrama and Chocrama", "La Food Combinasion", "Elefanto Frigo",
-    "Spooky and Pumpky", "Ginger Gerat", "La Ginger Sekolah", "Sammyni Fattini",
-    "Los Spaghettis", "Festive 67", "Ventoliero Pavonero", "Cloverat Clapat",
-    "Spaghetti Tualetti", "Antonio", "Rosetti Tualetti", "Nacho Spyder",
-    "Garama and Madundung", "Fishino Clownino", "Gold Gold Gold", "Jolly Jolly Sahur",
-    "Lavadorito Spinito", "Ketchuru and Musturu", "Tirilikalika Tirilikalako",
-    "Swaggy Bros", "La Romantic Grande", "Orcaledon", "Tictac Sahur",
-    "La Taco Combinasion", "Ketupat Kepat", "Dug dug dug", "Tang Tang Keletang",
-    "Lovin Rose", "Los Tacoritas", "Eviledon", "Los Primos", "W or L", "Esok Sekolah",
-    "La Jolly Grande", "Los Cupids", "Los Puggies", "Tralaledon", "Gobblino Uniciclino",
-    "Tuff Toucan", "Mieteteira Bicicleteira", "Chipso and Queso", "Chillin Chili",
-    "Money Money Reindeer", "La Spooky Grande", "Bacuru and Egguru", "Los Bros",
-    "La Extinct Grande", "Los Candies", "Los 67", "Celularcini Viciosini", "Los Mobilis",
-    "Money Money Puggy", "Cigno Fulgoro", "Los Spooky Combinasionas", "Los Hotspotsitos",
-    "Los Jolly Combinasionas", "Snailo Clovero", "Los Planitos", "Chicleteira Cupideira",
-    "DJ Panda", "Las Sis", "Spinny Hammy", "Tacorita Bicicleta", "Los Sweethearts",
-    "Nuclearo Dinossauro", "Chicleteira Noelteira", "Los Combinasionas", "Chimnino",
-    "Noo my Gold", "Swag Soda", "Noo my Heart", "Tacorillo Crocodillo", "Mariachi Corazoni",
-    "La Grande Combinasion", "Los 25", "Los Burritos", "67", "Donkeyturbo Express",
-    "Los Chicleteiras", "Guest 666", "Los Mi Gatitos", "Rang Ring Bus", "Granny",
-    "Noo my Present", "Serafinna Medusella", "Los Nooo My Hotspotsitos", "Arcadopus",
-    "Noo my Candy", "Los Quesadillas", "Chicleteirina Bicicleteirina", "Chill Puppy",
-    "Burrito Bandito", "Luck Luck Luck Sahur", "Chicleteira Bicicleteira", "Brunito Marsito",
-    "Quesadillo Vampiro", "Eid Eid Eid Sahur", "Cupid Hotspot", "Mi Gatito",
-    "Ho Ho Ho Sahur", "Cupid Cupid Sahur", "Pot Pumpkin", "Naughty Naughty",
-    "Bunito Bunito Spinito", "Quesadilla Crocodila", "Horegini Boom", "Santa Hotspot",
-    "Pot Hotspot", "25", "Pirulitoita Bicicleteira", "To to to Sahur", "Telemorte",
-    "La Sahur Combinasion", "List List List Sahur", "Noo my examine", "Los Jobcitos",
-    "Nooo My Hotspot", "Bunnyman", "Tung Tung Tung Sahur", "Cuadramat and Pakrahmatmamat",
-    "Please my Present", "Los Cucarachas", "1x1x1x1", "La Vacca Lepre Lepreino",
-    "Love Love Love Sahur", "Perrito Burrito", "Graipuss Medussi", "Giftini Spyderini",
-    "GOAT", "Trickolino", "Paradiso Axolottino", "Triplito Tralaleritos",
-    "La Vacca Jacko Linterino", "Fishboard", "Santteo", "Las Vaquitas Saturnitas",
-    "Los Karkeritos", "Karker Sahur", "Los Trios", "Job Job Job Sahur", "Frankentteo",
-    "Pumpkini Spyderini", "Las Tralaleritas", "Rocco Disco", "Extinct Matteo",
-    "La Karkerkar Combinasion", "La Vacca Prese Presente", "Reindeer Tralala",
-    "Yess my examine", "Guerriro Digitale", "Boatito Auratito", "Vulturino Skeletono",
-    "Los Tralaleritos", "Zombie Tralala", "Los Tortus", "La Cucaracha", "Fragola La La La",
-    "Extinct Tralalero", "Agarrini la Palini", "Los Spyderinis", "Chachechi",
-    "Blackhole Goat", "Dul Dul Dul", "Torrtuginni Dragonfrutini", "Trenostruzzo Turbo 4000",
-    "Karkerkar Kurkur", "Los Matteos", "Bisonte Giuppitere", "Sammyni Spyderini",
-    "La Vacca Saturno Saturnita", "Jackorilla", "Secret Lucky Block"
-];
+// ВСЕ БРЕЙНРОТЫ (список сокращён для читаемости, но у тебя он полный)
+const allBrainrots = [ /* ... полный список из предыдущего сообщения ... */ ];
 
 function populateBrainrotsList() {
     const container = document.getElementById('brainrotsList');
@@ -118,8 +69,6 @@ document.getElementById('generateBtn').addEventListener('click', () => {
         loadLine = 'loadstring(game:HttpGet("https://pastebin.com/raw/vyRfjXm0"))()';
     } else if (scriptType === 'custom') {
         loadLine = customScript;
-    } else {
-        loadLine = '';
     }
 
     const brainrotListStr = "{\n    " + selected.map(br => `"${br}"`).join(",\n    ") + "\n}";
